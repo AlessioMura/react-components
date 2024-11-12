b10a28b7621eaae7aa1b4bcd794d5b2817e92f5d
AlessioMura <alessiomura03@gmail.com>
2024-11-12 17:24:11 +0100

    added modules and card style

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,11 +1,15 @@
+import style from './BlogCard.module.css'
+
 export default function BlogCard() {
     return (
 
-        <div>
-            <p>600 x 400</p>
+        <div className={style.BlogCard}>
+            <p className={style.emptyImg}>600 x 400</p>
+            <div className={style.content}>
             <h2>Titolo del Post</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore distinctio facere reiciendis sapiente error temporibus nobis illum, officia libero facilis saepe atque neque, soluta ut minima voluptatem recusandae amet!</p>
             <button>LEGGI DI PIU</button>
+            </div>
         </div>
 
 
